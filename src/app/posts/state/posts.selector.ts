fe0cea2ad1f1637d2118a3c4499e4bc75ef6bfaf
Manish Sahu <manissh7352@gmail.com>
2022-04-13 19:06:52 +0530

    added prost edit form
    Props in Ngrx Selectors. Sending arguments to the state selectors for updating Post

--- src/app/posts/state/posts.selector.ts
+++ src/app/posts/state/posts.selector.ts
@@ -6,3 +6,7 @@ const getPostsState = createFeatureSelector<PostsState>('posts');
 export const getPosts = createSelector(getPostsState, (state) => {
   return state.posts;
 });
+
+export const getPostById = createSelector(getPostsState, (state, props) => {
+  return state.posts[props.id - 1] ? state.posts[props.id - 1] : null;
+});
